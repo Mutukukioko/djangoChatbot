98dd9c8bd844e626a9135c2c778f5d0d19ae39f3
Mutukukioko <kharrylungu@gmail.com>
2024-09-11 03:25:20 -0700

    url correction

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MessageView,QuestionAnswerView,ChatbotAnswerView
 
 urlpatterns = [
-    path('message/', QuestionAnswerView.as_view(), name='message'),
+    path('message/', MessageView.as_view(), name='message'),
      path('qanswer/', QuestionAnswerView.as_view(), name='qanswer'),
      path('askme/', ChatbotAnswerView.as_view(), name='askme'),
 ]
